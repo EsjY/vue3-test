3746f325cb34b20dbd30995236679d6e5c875b57
paas-ta0812 <73098573+paas-ta0812@users.noreply.github.com>
2022-01-20 16:04:07 +0900

    'Fruit'

---
+++ src/components/Fruit.vue
@@ -0,0 +1,21 @@
+<template>
+  <li>{{ name }}?!</li>  
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      defualt: ''
+    }
+  }
+}
+
+</script>
+
+<style scoped lang="scss">
+  h1 {
+    color: red !important;
+  }
+</style>
